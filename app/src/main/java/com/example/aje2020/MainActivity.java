35f1a275f7634a0e10986814d94d713eed85dcaa
Fares <faresinside@gmail.com>
2020-10-08 11:46:07 +0200

    Atelier Java Embarqué 2020

---
+++ app/src/main/java/com/example/aje2020/MainActivity.java
@@ -0,0 +1,45 @@
+
+
+package com.example.aje2020;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Sample Activity demonstrating how to connect to the network and fetch raw
+ * HTML. It uses a Fragment that encapsulates the network operations on an AsyncTask.
+ *
+ * This sample uses a TextView to display output.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        else {
+            // Permission has already been granted
+            Log.d("MAIN_ACTIVITY", "WRITE PERMISSION ALREADY GRANTED");
+        }
+    }
+
+    public void goToServerActivity(View view) {
+        Intent intent = new Intent(this, Server.class);
+        startActivity(intent);
+    }
+
+    public void goToClientActivity(View view) {
+        Intent intent = new Intent(this, DeviceList.class);
+        startActivity(intent);
+    }
+}
